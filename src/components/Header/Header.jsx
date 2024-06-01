51a6f7125aa5d8186c488f812302ad9afc30d610
pablo <tonyp2hoy@gmail.com>
2024-06-01 12:33:05 -0400

    feat: header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="wrapper">
+        <div className="cta">
+          <p className="course-name">Sound Design Masterclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <a href="#" className="demo-btn">
+            Demo Lesson
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
